f6b37f741c0caffdd511fd7ac1b531b034199a33
Zack Buhman <zack@buhman.org>
2017-06-08 22:31:25 -0500

    queue: add queue_manager helpers
    This adds a manager class to encapsulate queue state.

--- microscopic/queue.py
+++ microscopic/queue.py
@@ -1,6 +1,11 @@
 import asyncio
+from collections import deque
 
-from typing import AsyncGenerator, Deque, Union
+from typing import AsyncGenerator, Callable, Deque, Union
+
+
+Queue = Deque[Union[asyncio.Future, object]]
+MatchProcessor = Callable[[memoryview], Union[bytes, bytearray]]
 
 
 def unwrap(future):
@@ -10,7 +15,7 @@ def unwrap(future):
     return future.result()
 
 
-async def pump_queue(queue: Deque[Union[asyncio.Future, str]], *, wait: bool=False) -> AsyncGenerator[str, None]:
+async def pump_queue(queue: Queue, *, wait: bool=False) -> AsyncGenerator[str, None]:
     while True:
         try:
             item = queue.popleft()
@@ -27,3 +32,30 @@ async def pump_queue(queue: Deque[Union[asyncio.Future, str]], *, wait: bool=Fal
         else:
             queue.appendleft(item)
             break
+
+
+def push_queue_pattern(strip: memoryview, match: memoryview, *,
+                       queue: Queue, match_processor: MatchProcessor):
+    assert strip or match
+
+    if strip:
+        queue.append(strip)
+    if match:
+        queue.append(
+            asyncio.ensure_future(
+                match_processor(match)
+            ))
+
+
+class PatternQueueManager:
+    def __init__(self, match_processor: MatchProcessor, queue_factory: Callable[[], Deque]=deque):
+        self.queue = queue_factory()
+        self.match_processor = match_processor
+
+    def push(self, strip, match):
+        push_queue_pattern(strip, match,
+                           queue=self.queue, match_processor=self.match_processor)
+
+    async def pump(self, wait: bool=False) -> memoryview:
+        async for data in pump_queue(self.queue, wait=wait):
+            yield data
